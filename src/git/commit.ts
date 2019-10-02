f9c30567fdb59cd9fcbfd93ccba8ccaad67599c1
Jack Ellis <jack.ellis@godaddy.com>
2019-10-02 14:52:59 +0100

    fix: typo on commit command

--- src/git/commit.ts
+++ src/git/commit.ts
@@ -4,5 +4,5 @@ export type Commit = (message: string) => Promise<void>;
 
 export default (exec: Exec): Commit => async(message) => {
   await exec('git add .');
-  await exec(`git commit -m "${message} --no-verify`);
+  await exec(`git commit -m "${message}" --no-verify`);
 };
